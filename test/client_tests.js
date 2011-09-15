562e4e2acfe47e4e99c748019b5a97c5e808e987
Dennis Lipovsky <den@denlipovsky.com>
2011-09-14 21:31:39 -0400

    setting up the client tests to mock out all the UI related and IO related stuff
    --HG--
    branch : test-refactor

--- test/client_tests.js
+++ test/client_tests.js
@@ -1,8 +1,31 @@
-io = {};
+var handlers = {};
+var socket = { sent: [], on: function(msg, fn) { handlers[msg] = fn; }, emit: function(msg) { this.sent.push(msg); } };
+io = { connect: function() { return socket; } };
+
+UI = { messages: [], init: function() { }, showMsg: function(msg) { this.messages.push(msg); } };
+
 var assert = require('assert');
 var c4 = require('../c4');
+
+var before = function() {
+    c4.Engine.init();
+};
+
 module.exports = {
-    'test working': function() {
-        assert.isNotNull(c4.UI);
+    'client engine object is defined': function() {
+        assert.isDefined(c4.Engine);
+    },
+    
+    'engine initialization sets up the game': function() {
+        before();
+        assert.length(socket.sent, 1);
+        assert.includes(socket.sent, 'available');
+    },
+    
+    'on error show the message in the ui': function() {
+        before();
+        handlers.error({msg: 'error'});
+        assert.length(UI.messages, 1);
+        assert.includes(UI.messages, 'error');
     }
 };
